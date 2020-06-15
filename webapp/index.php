07ccba78fd4c9d1cfbf82852937563a6ed4e961a
DDeGonge888 <DDeGonge888@gmail.com>
2020-06-14 20:31:43 -0400

    no clue what im doing

--- webapp/index.php
+++ webapp/index.php
@@ -1,37 +1,30 @@
-﻿<!DOCTYPE html>
-<!--TheFreeElectron 2015, http://www.instructables.com/member/TheFreeElectron/ -->
-
 <html>
-    <head>
-        <meta charset="utf-8" />
-        <title>Raspberry Pi Gpio</title>
-    </head>
- 
-    <body style="background-color: black;">
-    <!-- On/Off button's picture -->
-	<?php
-	$val_array = array(0,0,0,0,0,0,0,0);
-	//this php script generate the first page in function of the file
-	for ( $i= 0; $i<8; $i++) {
-		//set the pin's mode to output and read them
-		system("gpio mode ".$i." out");
-		exec ("gpio read ".$i, $val_array[$i], $return );
-	}
-	//for loop to read the value
-	$i =0;
-	for ($i = 0; $i < 8; $i++) {
-		//if off
-		if ($val_array[$i][0] == 0 ) {
-			echo ("<img id='button_".$i."' src='data/img/red/red_".$i.".jpg' onclick='change_pin (".$i.");'/>");
-		}
-		//if on
-		if ($val_array[$i][0] == 1 ) {
-			echo ("<img id='button_".$i."' src='data/img/green/green_".$i.".jpg' onclick='change_pin (".$i.");'/>");
-		}	 
-	}
-	?>
-	 
-	<!-- javascript -->
-	<script src="script.js"></script>
-    </body>
+	<head>
+	</head>
+
+	<body>
+		
+		<iframe name="hidden_iframe" width="0" height="0" style="border:none"></iframe>
+		<form method="post" action="out.php" target="hidden_iframe">
+			
+	        <input type="submit" name="foo" value="A" />
+			<input type="submit" name="foo" value="B" />
+			<input type="submit" name="foo" value="C" />
+			<input type="submit" name="foo" value="D" />
+			<input type="submit" name="foo" value="E" />
+			<input type="submit" name="foo" value="F" />
+			<input type="submit" name="foo" value="G" />
+		
+		</form>
+		
+		<?php
+		$name = $_POST['foo'];
+		$fp = fopen("formdata.txt", "w");
+		fwrite($fp, "");
+		$savestring = $name;
+		fwrite($fp, $savestring);
+		fclose($fp);
+		?>
+		
+	</body>
 </html>
